d2479a51124bc0a259fbc5481fca2a10277a7b2f
vothituongvy <Vothituongvyvttv@gmail.com>
2023-08-18 12:17:06 +0700

    nop bai tap

--- module2/src/ss3_mang_va_phuong_thuc_java/ThemPhanTuVaoMang.java
+++ module2/src/ss3_mang_va_phuong_thuc_java/ThemPhanTuVaoMang.java
@@ -4,19 +4,27 @@ import java.util.Scanner;
 
 public class ThemPhanTuVaoMang {
     public static void main(String[] args) {
-        int[] arr = {10, 4, 6, 7, 8, 0, 0, 0, 0, 0};
         Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap vao so luong phan tu trong mang: ");
+        int a = Integer.parseInt(sc.nextLine());
+        int[] arr = new int[a];
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.print("Nhap phan tu tai index " + i + " : ");
+            int b = Integer.parseInt(sc.nextLine());
+            arr[i] = b;
+        }
         System.out.println("Nhập số cần chèn");
         int x = sc.nextInt();
         System.out.println("Nhập vào vị trí cần chèn");
         int index = sc.nextInt();
-        if (index <= 1 || index >= arr.length - 1) {
-            System.out.println("Không thể chèn phần tử " + x + " vào mảng");
+        if (arr[index] == 0) {
+            arr[index] = x;
+        } else {
+            for (int i = arr.length - 1; i > index; i--) {
+                arr[i] = arr[i - 1];
+            }
+            arr[index] = x;
         }
-        for (int i = arr.length - 1; i > index; i--) {
-            arr[i] = arr[i - 1];
-        }
-        arr[index] = x;
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
--- module2/src/ss3_mang_va_phuong_thuc_java/XoaPhanTuKhoiMang.java
+++ module2/src/ss3_mang_va_phuong_thuc_java/XoaPhanTuKhoiMang.java
@@ -6,19 +6,16 @@ import java.util.Scanner;
 public class XoaPhanTuKhoiMang {
     public static void main(String[] args) {
         int arr[] = {10, 4, 6, 7, 8, 6, 0, 0, 0};
-        int index_del = -1;
+        int indexDel = -1;
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhập phần tử cần xóa:");
         int x = sc.nextInt();
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == x) {
-                index_del = i;
-                break;
-            }
-        }
-        if (index_del != -1) {
-            for (int i = index_del; i < arr.length - 1; i++) {
-                arr[i] = arr[i + 1];
+            if (arr[i]==x) {
+                indexDel=i;
+                for (int j = indexDel; j < arr.length - 1; j++) {
+                    arr[j] = arr[j + 1];
+                }
                 arr[arr.length - 1] = 0;
             }
         }
