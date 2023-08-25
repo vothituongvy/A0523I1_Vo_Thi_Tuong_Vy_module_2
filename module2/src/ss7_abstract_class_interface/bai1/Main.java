0756f57666900d32e7c1deecdda37766b3f83865
vothituongvy <Vothituongvyvttv@gmail.com>
2023-08-25 18:09:54 +0700

    nop bai tap

--- module2/src/ss7_abstract_class_interface/bai1/Main.java
+++ module2/src/ss7_abstract_class_interface/bai1/Main.java
@@ -8,15 +8,15 @@ public class Main {
         shapes[0] = new Circle(5);
         shapes[1] = new Rectangle(4, 6);
         shapes[2] = new Square(3);
-
+        //Random
         Random random = new Random();
 
         for (IResizeable shape : shapes) {
             double percent = random.nextInt(100) + 1;
             double originalArea = shape.getArea();
+            //Dùng để thay đổi kích thước hình dạng theo tỉ lệ phần trăm
             shape.resize(percent);
             double newArea = shape.getArea();
-
             System.out.println("Diện tích ban đầu: " + originalArea);
             System.out.println("Diện tích sau khi tăng kích thước: " + newArea);
         }
--- module2/src/ss7_abstract_class_interface/bai2/Test.java
+++ module2/src/ss7_abstract_class_interface/bai2/Test.java
@@ -2,7 +2,7 @@ package ss7_abstract_class_interface.bai2;
 
 public class Test {
     public static void main(String[] args) {
-        Squares color =new Squares();
+        Squares color = new Squares();
         color.howToColor();
     }
 }
