3233ff4a6f732db69e8be9e4af0845c386fbb2bd
vothituongvy <Vothituongvyvttv@gmail.com>
2023-09-20 18:02:33 +0700

    nop bai tap

--- module2/src/ss13_thuat_toan_tim_kiem/TimChuoiLienTiepCoDaiLonNhat.java
+++ module2/src/ss13_thuat_toan_tim_kiem/TimChuoiLienTiepCoDaiLonNhat.java
@@ -1,5 +1,28 @@
 package ss13_thuat_toan_tim_kiem;
 
-public class TimChuoiLienTiepCoDaiLonNhat {
+import java.util.LinkedList;
+import java.util.Scanner;
 
+public class TimChuoiLienTiepCoDaiLonNhat {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        LinkedList<Character> max=new LinkedList<>();
+        LinkedList<Character> list=new LinkedList<>();
+        System.out.println("Nhập chuổi: ");
+        String string=sc.nextLine();
+        for (int i = 0; i < string.length(); i++) {
+            if(list.size()>1 && string.charAt(i)<= list.getLast() && list.contains(string.charAt(i))){
+                list.clear();
+            }
+            list.add(string.charAt(i));
+            if(list.size()>max.size()){
+                max.clear();
+                max.addAll(list);
+            }
+        }
+        for (Character character:max) {
+            System.out.print(character);
+        }
+        System.out.println();
+    }
 }
